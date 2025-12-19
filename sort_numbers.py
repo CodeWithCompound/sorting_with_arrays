b5e182ea03a192b37f469246619c221ab12b4cac
CodeWithCompound <mathematico.loco@gmail.com>
2025-12-19 17:05:14 +0100

    cleaned up code

--- sort_numbers.py
+++ sort_numbers.py
@@ -1,7 +1,4 @@
-print("Sorting Numbers Script")
-print("=======================")
 unsorted_numbers = [34, 1, 23, 4, 3, 78, 111, 88, 12, 56]
-print("Unsorted Numbers:", unsorted_numbers)
 
 def slice(unsorted_numbers):
     sliced_a = unsorted_numbers[:len(unsorted_numbers)//2]
@@ -9,16 +6,20 @@ def slice(unsorted_numbers):
     return sliced_a, sliced_b
 
 def sort(numbers):
-    # Simple sorting using built-in sorted function
-    # Tho i planught of implementing my own sorting algorithm later
     return sorted(numbers)
-
+# note
 length = len(unsorted_numbers)
 sliced_a, sliced_b = slice(unsorted_numbers)
 
+print("Sorting Numbers Script")
+print("=======================")
+print("Unsorted Numbers:", unsorted_numbers)
 print("========================")
 print("Sliced A:", sliced_a)
 print("Sliced B:", sliced_b)    
 print("========================")
 print("Sorted Numbers:", sort(sliced_a + sliced_b))
+print("========================")
+print("Using: Built-in sorted() function")
 
+# note: Simple sorting using built-in sorted function. Tho i planught of implementing my own sorting algorithm later
